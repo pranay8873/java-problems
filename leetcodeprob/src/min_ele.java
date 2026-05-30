3feacef307cdc126f09a1547d0c21614b5734fef
pranay8873 <pranaypuvaati@gmail.com>
2026-05-30 23:03:37 +0530

    leetcode problem 1

--- leetcodeprob/src/min_ele.java
+++ leetcodeprob/src/min_ele.java
@@ -1,3 +1,31 @@
+//You are given an integer array nums.
+//
+//You replace each element in nums with the sum of its digits.
+//
+//Return the minimum element in nums after all replacements.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [10,12,13,14]
+//
+//Output: 1
+//
+//Explanation:
+//
+//nums becomes [1, 3, 4, 5] after all replacements, with minimum element 1.
+//
+//Example 2:
+//
+//Input: nums = [1,2,3,4]
+//
+//Output: 1
+//
+//Explanation:
+//
+//nums becomes [1, 2, 3, 4] after all replacements, with minimum element 1.
+
 public class min_ele {
     public int minElement(int[] nums) {
         int minSum = Integer.MAX_VALUE;
