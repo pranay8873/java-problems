c46c99bba70681bfbefe9c7c12a4958c3aee3b80
pranay8873 <pranaypuvaati@gmail.com>
2026-06-01 22:46:21 +0530

    candies problem

--- leetcodeprob/src/candies_problem.java
+++ leetcodeprob/src/candies_problem.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 //
 //Input: cost = [1,2,3]
 //Output: 5
+//Explanation: The customer can buy the first two candies for a total cost of 3 and take the third candy for free.
 public class candies_problem {
     {
         public int minimumCost(int[] cost) {
