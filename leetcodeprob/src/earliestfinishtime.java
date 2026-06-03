780f96e7b604193cc69f175e7e74e7fbc75a6d8e
pranay8873 <pranaypuvaati@gmail.com>
2026-06-03 23:26:07 +0530

    added discreption

--- leetcodeprob/src/earliestfinishtime.java
+++ leetcodeprob/src/earliestfinishtime.java
@@ -1,3 +1,25 @@
+//You are given two categories of theme park attractions: land rides and water rides.
+//
+//        Land rides
+//landStartTime[i] – the earliest time the ith land ride can be boarded.
+//landDuration[i] – how long the ith land ride lasts.
+//Water rides
+//waterStartTime[j] – the earliest time the jth water ride can be boarded.
+//waterDuration[j] – how long the jth water ride lasts.
+//A tourist must experience exactly one ride from each category, in either order.
+//
+//A ride may be started at its opening time or any later moment.
+//If a ride is started at time t, it finishes at time t + duration.
+//Immediately after finishing one ride the tourist may board the other (if it is already open) or wait until it opens.
+//Return the earliest possible time at which the tourist can finish both rides.
+//
+//
+//
+//        Example 1:
+//
+//Input: landStartTime = [2,8], landDuration = [4,1], waterStartTime = [6], waterDuration = [3]
+//
+//Output: 9
 public class earliestfinishtime {
     import java.util.*;
 
