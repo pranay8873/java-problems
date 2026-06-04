c08cf598a87e026c28c17a0ae8fef3be6e51ac0d
pranay8873 <pranaypuvaati@gmail.com>
2026-06-04 22:48:45 +0530

    discription

--- leetcodeprob/src/Total_waveness.java
+++ leetcodeprob/src/Total_waveness.java
@@ -1,3 +1,13 @@
+//You are given two integers num1 and num2 representing an inclusive range [num1, num2].
+//
+//The waviness of a number is defined as the total count of its peaks and valleys:
+//
+//A digit is a peak if it is strictly greater than both of its immediate neighbors.
+//A digit is a valley if it is strictly less than both of its immediate neighbors.
+//The first and last digits of a number cannot be peaks or valleys.
+//Any number with fewer than 3 digits has a waviness of 0.
+//Return the total sum of waviness for all numbers in the range [num1, num2].
+
 public class Total_waveness {
 
         public int totalWaviness(int num1, int num2) {
